da3332a15dcb48261eb5facac83fe9bc047aa1c8
Yudeat <ayushtiwaei0@gmail.com>
2025-11-06 22:32:07 +0545

    The Dash Board elements changed

--- app/page.tsx
+++ app/page.tsx
@@ -12,33 +12,34 @@ const Page = () => {
 
   const items: CardNavItem[] = [
     {
-      label: 'About',
-      bgColor: '#0D0716',
+      label: 'Stories',
+      bgColor: '#312d64ff',
       textColor: '#fff',
       links: [
-        { label: 'Company', ariaLabel: 'About Company', href: '/company' },
-        { label: 'Careers', ariaLabel: 'About Careers', href: '/careers' },
+        { label: 'All Stories', ariaLabel: 'About Company', href: '/company' },
+        { label: 'Trending', ariaLabel: 'About Careers', href: '/careers' },
       ],
     },
     {
-      label: 'Projects',
+      label: 'Poems',
       bgColor: '#170D27',
       textColor: '#fff',
       links: [
-        { label: 'Featured', ariaLabel: 'Featured Projects', href: '/projects/featured' },
-        { label: 'Case Studies', ariaLabel: 'Project Case Studies', href: '/projects/case-studies' },
+        { label: 'All Poems', ariaLabel: 'Featured Projects', href: '/projects/featured' },
+        { label: 'Trending', ariaLabel: 'Project Case Studies', href: '/projects/case-studies' },
       ],
     },
     {
-      label: 'Contact',
+      label: 'About',
       bgColor: '#271E37',
       textColor: '#fff',
       links: [
-        { label: 'Email', ariaLabel: 'Email us', href: 'mailto:poemsbits@gmail.com' },
-        { label: 'Twitter', ariaLabel: 'Twitter', href: 'https://twitter.com/example' },
-        { label: 'LinkedIn', ariaLabel: 'LinkedIn', href: 'https://linkedin.com/company/example' },
+        { label: 'Contact', ariaLabel: 'Email us', href: 'mailto:poemsbits@gmail.com' },
+        { label: 'Email', ariaLabel: 'Twitter', href: 'https://twitter.com/example' },
+        { label: 'Address', ariaLabel: 'LinkedIn', href: 'https://linkedin.com/company/example' },
       ],
     },
+    
   ];
 
   return (
