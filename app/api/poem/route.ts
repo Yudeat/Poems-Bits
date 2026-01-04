1a7caef492c8a858ebb5a295630af55430c233d0
Yudeat <ayushtiwaei0@gmail.com>
2026-01-04 21:11:11 +0545

    y

--- app/api/poem/route.ts
+++ app/api/poem/route.ts
@@ -25,7 +25,7 @@ export async function POST(req: Request) {
 
     let clerkUser;
     try {
-      clerkUser = await clerkClient.users.getUser(userId);
+      clerkUser = await createclerkClient.users.getUser(userId);
     } catch {
       clerkUser = { firstName: "", emailAddresses: [] };
     }
