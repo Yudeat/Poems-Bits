c35ded8f192342daa7a93764c0c376b3bf79624d
Yudeat <ayushtiwaei0@gmail.com>
2026-02-21 22:11:52 +0545

    next config updated

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+output: 'export', // This is the magic line for GitHub Pages
+  images: {
+    unoptimized: true, // Necessary because GitHub Pages doesn't support Next.js Image Optimization
+  },};
 
 export default nextConfig;
